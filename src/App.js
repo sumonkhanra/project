e2b05b2560f6edcc51c967760504888b7b89499d
sumonkhanra <khanrasumon7@gmail.com>
2023-06-11 12:15:21 +0530

    'update'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import Assignment from './Assignment';
+import Multi from './Multi';
+
+function App() {
+  return (
+    <>
+<Multi/>
+{/* <Assignment/> */}
+</>
+  );
+}
+
+export default App;
